1d4d41cb13ac51b55c3f4bbcb28d39283f837900
Marosak <113629317+marosak09@users.noreply.github.com>
2024-02-04 14:03:36 +0100

    Update remove_warn_role.py

--- cogs/remove_warn_role.py
+++ cogs/remove_warn_role.py
@@ -1,43 +1,55 @@
 import discord
 from discord.ext import commands
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-
+from firebase_admin import credentials, db
 
 # firebase setup
 cred = credentials.Certificate("key.json")
 
 # Realtime database initialization
-ref = db.reference('/')
-ref_msglog = db.reference('/settings/msglog')
-ref_custom_commands = db.reference('/custom_commands')
-ref_answers = db.reference('/answers')
+intents = discord.Intents.default()
+intents.message_content = True
 
+client = commands.Bot(command_prefix=".", intents=intents)
+client.remove_command('help')
 
-class remove_warn_role(commands.Cog):
+index = "."
+
+def get_guild_ref(guild_id):
+    return db.reference(f'servers/{guild_id}')
+
+class RemoveWarnRole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        self.role_to_remove = None  # Přidáváme globální proměnnou pro uchování nastavené role
 
     @commands.command()
-    async def remove_warn_role(self, ctx, role: discord.Role):
-      second_warn_role_ref = db.reference('/settings/second_warn_role')
-      if second_warn_role_ref.get():
-        old_second_warn_role_id = second_warn_role_ref.get()
-        old_second_warn_role = ctx.guild.get_role(old_second_warn_role_id)
-        if old_second_warn_role:
-          await ctx.send(
-            f"Replacing second warn role from {old_second_warn_role.mention} to {role.mention}"
-          )
-        else:
-          await ctx.send(f"Setting second warn role to {role.mention}")
-      second_warn_role_ref.set(role.id)
-      await ctx.send(f"Second Warn role set to {role.mention}")
+    async def role_to_remove(self, ctx, role: discord.Role):
+        guild_id = ctx.guild.id
+        guild_ref = get_guild_ref(guild_id)
 
+        role_to_remove_ref = guild_ref.child('settings/role_to_remove')
+
+        if role_to_remove_ref.get():
+            old_role_to_remove_id = role_to_remove_ref.get()
+            old_role_to_remove = ctx.guild.get_role(old_role_to_remove_id)
+
+            if old_role_to_remove:
+                await ctx.send(
+                    f"Replacing second warn role from {old_role_to_remove.mention} to {role.mention}"
+                )
+            else:
+                await ctx.send(f"Setting second warn role to {role.mention}")
+
+        self.role_to_remove = role
+        save_config(guild_id, self.role_to_remove)
+
+        await ctx.send(f"Second Warn role set to {role.mention}")
+
+def save_config(guild_id, role_to_remove):
+    if role_to_remove:
+        guild_ref = get_guild_ref(guild_id)
+        ref_role_to_remove = guild_ref.child('settings/role_to_remove')
+        ref_role_to_remove.set(role_to_remove.id)
 
 async def setup(bot):
-    await bot.add_cog(remove_warn_role(bot))
-
-
-
+    await bot.add_cog(RemoveWarnRole(bot))
